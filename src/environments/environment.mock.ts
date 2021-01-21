58e0dcc429b654cc1e85a1e15154b56b74d5d922
Eugene Kolomiiets <kolomiets1990@gmail.com>
2021-01-21 16:10:38 +0200

    fixes paths

--- src/environments/environment.mock.ts
+++ src/environments/environment.mock.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    backend: 'https://app-news-server.herokuapp.com/news'
+    backend: 'https://app-news-server.herokuapp.com'
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  backend: 'https://app-news-server.herokuapp.com/news'
+  backend: 'https://app-news-server.herokuapp.com'
 };
 
 /*
